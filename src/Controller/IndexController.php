a34705e572a8403e1e874d7d978b3f7f595a8ac1
Madické THIANE <madicke.thiane@gmail.com>
2021-06-15 16:11:03 +0200

    15/06/2021 à 16:10

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        $autre_variable = "Hello";
+        return $this->render('index/index.html.twig', [
+            'controller_name' => '{Ceci est une variable Twig}',
+            'autre_variable' => $autre_variable,
+        ]);
+    }
+
+    /**
+     * @Route("/response/{option}", name="index_response")
+     */
+    public function indexResponse(Request $request, $option): Response
+    {
+        //Ctrl + clic droit -> Import All Classes
+
+        switch ($option) {
+            case 'rouge':
+                $figure = 'red';
+                break;
+            case 'vert':
+                $figure = 'green';
+                break;
+            case 'bleu':
+                $figure = 'blue';
+                break;
+            default:
+                $figure = "black";
+        }
+
+        return new Response("<div style='width:200px; height:300px; background-color : " . $figure . ";'></div>");
+        // return new Response($option);
+    }
+}
