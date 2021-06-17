b903bdc3a045e2ab73c5fe1ec7712c66a0b44377
Madické THIANE <madicke.thiane@gmail.com>
2021-06-17 16:36:26 +0200

    Ajout de la classe BulletinFixtures

---
+++ src/DataFixtures/BulletinFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Bulletin;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class BulletinFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $bulletin = new Bulletin;
+        $bulletin->setTitle('Bleu');
+        $bulletin->setCategory("General");
+        $bulletin->setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+        $manager->flush();
+    }
+}
